c73d064f33b43e704a071b78d6e9c4abea474e20
haeter525 <cindejze412@gmail.com>
2023-05-01 17:16:11 +0800

    Add tests for profitability

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,10 +5,20 @@ DATASET_PATHS = [
         "originalData": "data/adult.csv",
         "anonymizedData": "data/adult_anonymized.csv",
         "dataHierarchy": "data/adult_hierarchy",
-    }
+    },
+    {
+        "originalData": "data/delta.csv",
+        "anonymizedData": "data/delta_anonymized.csv",
+        "dataHierarchy": "data/delta_hierarchy",
+    },
 ]
 
 
 @pytest.fixture(scope="session")
 def DATASET_PATH_ADULT():
     return DATASET_PATHS[0]
+
+
+@pytest.fixture(scope="session")
+def DATASET_PATH_DELTA():
+    return DATASET_PATHS[1]
